856fb70450491320142c1bb832741eca4d833cb9
Dariusz Skiciak <dariusz.skiciak@gmail.com>
2017-12-19 21:46:40 +0100

    Feat: - add endpoint to login

---
+++ routes/loginRoute.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import randToken from 'rand-token';
+import Users from '../models/users';
+
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await Users.find({
+    where: {
+      email
+    }
+  });
+
+  if (!user || !user.vaildPassword(password)) {
+    res.status(403).json({ error: 'incorrect email or password' });
+    return;
+  }
+
+  const liveTimeToken = new Date(Date.now() + (12 * 3600000));
+  user.token = { token: randToken.generate(24), liveTime: liveTimeToken };
+
+  try {
+    await user.save();
+    const response = {
+      ...user
+    };
+    res.status(200).json({ data: response });
+  } catch (e) {
+    res.status(401).json({ error: 'forbidden' });
+  }
+});
